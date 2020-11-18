3a9f784f46d2ee7ce53016241c6e94542fd819e2
Franklin Lonberg <flonberg@gmail.com>
2020-11-18 17:35:30 -0500

    last of 11-18-2020

--- src/app/output-graph/output-graph.component.ts
+++ src/app/output-graph/output-graph.component.ts
@@ -96,34 +96,14 @@ export class OutputGraphComponent implements OnInit {
       enabled: false
     },
     xAxis: {
-      
       crosshair: true
   },
     tooltip: {
       formatter: function (){
-        return  Highcharts.dateFormat('%e %b %y %H:%M:%S', this .x) + " Duration:" + this .y + " minutes. "  ;
+        return  Highcharts.dateFormat('%e %b %y %H:%M:%S', this .x) +  this .y + " Plans. "  ;
       }
     },
-    
-    series: [
-      {
-      name: 'Tokyo',
-      data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
-
-  }, {
-      name: 'New York',
-      data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
-
-  }, {
-      name: 'London',
-      data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
-
-  }, {
-      name: 'Berlin',
-      data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
-
-  }
-]
+    series: [{}, {}, {}, {}]
   }
   constructor(private genSvce: GenService) {
     this .selected = "Treatment";
@@ -144,6 +124,7 @@ export class OutputGraphComponent implements OnInit {
           var i = 0;
           for (let key of Object.keys(res['Patients'])) {
             this .options.series[i] = [];
+          //  this .options.series2[i] = [];
             this .options.series[i]['name'] = key;
             this .options.series[i]['data'] = res['Patients'][key];
             this .options2.series[i]['name'] = key;
