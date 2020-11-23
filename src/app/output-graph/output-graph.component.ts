3e326cec3b0f6c87fe8700c75d00b3f22107c19f
Franklin Lonberg <flonberg@gmail.com>
2020-11-23 12:59:52 -0500

    before cleanup

--- src/app/output-graph/output-graph.component.ts
+++ src/app/output-graph/output-graph.component.ts
@@ -116,6 +116,7 @@ export class OutputGraphComponent implements OnInit {
         {},
       ]
     }
+
     public options2: any =
     {
       chart: {
@@ -129,14 +130,37 @@ export class OutputGraphComponent implements OnInit {
         crosshair: true,
         format: "test"
         },
+        yAxis: {
+          min: 0,
+          title: {
+              text: 'Total fruit consumption'
+          },
+          stackLabels: {
+              enabled: true,
+              style: {
+                  fontWeight: 'bold',
+                  color: ( // theme
+                      Highcharts.defaultOptions.title.style &&
+                      Highcharts.defaultOptions.title.style.color
+                  ) || 'gray'
+              }
+          }
+      },
+      plotOptions: {
+        column: {
+            stacking: 'normal',
+            dataLabels: {
+                enabled: false
+            }
+        }
+    },
       click: function (e) {
         console.log(
             Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', e.xAxis[0].value),
             e.yAxis[0].value
         )
       },
-      plotOptions: {
-      },
+   
         credits: {
           enabled: false
         },
@@ -211,83 +235,50 @@ export class OutputGraphComponent implements OnInit {
     var i = 0;
     var oLoopCount = 0;
     var keyCount = 0;
-    for (let key of Object.keys(this .data['Patients'])) {                    // loop through the Patients
 
-      console.log("212 key is %o", key);
-      for (let key2 of  this .data['Patients'][key] ){                        // loop thru the patients Session Durations
-        var binCount = 0;                                                     // the index of the bin, e.g first, second, third ...
-        for (let entry of this .binsC ){                                      // loop thry the bins
-
-          this .tst[binCount] = new Array();
-
-          if (  key2[1] > entry[0] && key2[1]   <= entry[1] ){                // if the Duration is in the bin
-      
-            if (!this .tst[binCount][key]){
-              this .tst[binCount][key] = 1;
-
-            }
-            this .numInBin[binCount]++                                        // increment the count in the bin
-
-          }
-          binCount++;                                                         // go to the next bin
-
-        }
-      }
-////////      Top Graph scatter plot       \\\\\\\\\\\\\\\\\\\
-      this .options.series[i] = [];
-      this .options.series[i]['name'] = key;
-      this .options.series[i]['data'] = this .data['Patients'][key];
-////////      Bottom Graph Histogram       \\\\\\\\\\\\\\\\\\\\
-   //   this .options2.series[0] = [];
-  //    this .options2.series[0]['name'] = 'Minutes';
-      this .options2.series = this .tst2;
-//      this .options2.series[0]['data'] = this .numInBin;
-      this .options2.xAxis['categories'] = this .binsC['Label'];
-      i++;
-
-    }
-/////////  make the bins for each patient  \\\\\\\\\\\\\\\\\\\\\    
+/////////  make the bins for each patient  \\\\\\\\\\\\\\\\\\\\\
     var patCount2 = 0;
     for (let key of Object.keys(this .data['Patients'])) {                    // loop through the Patients
       var tstObj = {'name': key, 'data': []}                                  // make an objest to hole the patient bin data
       this .tst2.push(tstObj);
       var binCount2 = 0;                                                       // push the object into the main array;
       for (let binEntry of this .binsC){
-   //     console.log("253 binEntry is %o", binEntry);
         this .tst2[patCount2].data[binCount2++] = 0
       }
       patCount2++;
     }
-    var patCount2 = 0;
+////////   bin the data  \\\\\\\\\\\\\\\\\\\\\\\
+    var patCount2 = 0;                                                        // patient loop counter
     for (let key of Object.keys(this .data['Patients'])) {                    // loop over patients
-
-    //  console.log("253 key is %o pardata is %o", key, this .data['Patients'][key])
       for (let entry of this .data['Patients'][key]) {                        // loop over each patient's durations
-   //     console.log(entry[1]); // 1, "string", false
-        var binCount2 = 0;
+        var binCount2 = 0;                                                    // loop counter
         for (let binEntry of this .binsC ){
-          if (entry[1] > binEntry[0] && entry[1] <= binEntry[1]){
-     //       console.log("257 bincC endty is %o duration is %o this bin has %o", binEntry[0],  entry[1], this .tst2)
-            this .tst2[patCount2]['data'][binCount2]++
+          if (entry[1] > binEntry[0] && entry[1] <= binEntry[1]){             // if duration is withing the bin limits
+            this .tst2[patCount2]['data'][binCount2]++                        // increment the count in that bin
           }
           binCount2++;
         }
       }
       patCount2++;
-
-    //  break;
     }
-    console.log("tst2 is %o ", this .tst2)
- 
+          ////////     Load data into  Top Graph scatter plot       \\\\\\\\\\\\\\\\\\\
+    for (let key of Object.keys(this .data['Patients'])) {                    // loop through the Patients
+            this .options.series[i] = [];
+            this .options.series[i]['name'] = key;
+            this .options.series[i]['data'] = this .data['Patients'][key];
+            i++;
+          }
+    ////////    Load data into Bottom Graph Histogram       \\\\\\\\\\\\\\\\\\\\
+    this .options2.series = this .tst2;
+    this .options2.xAxis['categories'] = this .binsC['Label'];
   }
-
+///////////  create the bins for the selected binSize. 
   makeBins(){
+    var maxDurationExpected = 60;                                             // set the maximum expected activityID duration
     this .binsC = [];                                                         // create the array of bins
     this .numInBin = [];
-
-
     this .binsC['Label'] = [];
-    var numBins = 60 / this .binSizeC;                                        // create max number of bins = longestExpectedTime / numBins
+    var numBins = maxDurationExpected / this .binSizeC;                      // create max number of bins = longestExpectedTime / numBins
     for (let i = 0; i < numBins; i++) {
       this .binsC[i] = [i * this .binSizeC, (i + 1) * this .binSizeC];        // set lower and upper bounds for each bin.
       this .binsC['Label'][i] = i * this .binSizeC + " to " +( i + 1) * this .binSizeC;   // the label for the bin in the graph
@@ -295,21 +286,4 @@ export class OutputGraphComponent implements OnInit {
       this .numInBin[i] = 0;                                                  // zero out the count in each bin. 
     }
    }
-   bins2C: any;
-   num2InBin = [[]];
-   patCount = 4;
- make2dBins(){
-  var numBins = 60 / this .binSizeC;                                        // create max number of bins = longestExpectedTime / numBins
-  this .bins2C =[this .numInBin] ;                                                         // create the array of bins
-  this .num2InBin = new Array(new Array());
-    for (let i = 0; i < numBins; i++) {
-      for (let j = 0; j < this .patCount; j++ ){
-        if (!this .bins2C[i])
-          this .bins2C[i] = new Array();
-          this .bins2C[i][j]  = [i * this .binSizeC, (i + 1) * this .binSizeC];        // set lower and upper bounds for each bin.
-//          this .bins2C[i][j]  = [i * this .binSizeC, (i + 1) * this .binSizeC];        // set lower and upper bounds for each bin.
-        }
-      }
-      console.log("254  bins2C is %o", this .bins2C)
- }
 }
